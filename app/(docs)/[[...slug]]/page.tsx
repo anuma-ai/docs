dcc6ed7f3b9bdd3ab599705c5986a87757db9ce4
Denis Fadeev <denis@fadeev.org>
2025-11-11 16:54:52 +0300

    docs in root path

--- app/(docs)/[[...slug]]/page.tsx
+++ app/(docs)/[[...slug]]/page.tsx
@@ -1,16 +1,18 @@
-import { getPageImage, source } from '@/lib/source';
+import { getPageImage, source } from "@/lib/source";
+import { getMDXComponents } from "@/mdx-components";
 import {
   DocsBody,
   DocsDescription,
   DocsPage,
   DocsTitle,
-} from 'fumadocs-ui/page';
-import { notFound } from 'next/navigation';
-import { getMDXComponents } from '@/mdx-components';
-import type { Metadata } from 'next';
-import { createRelativeLink } from 'fumadocs-ui/mdx';
+} from "fumadocs-ui/page";
+import { createRelativeLink } from "fumadocs-ui/mdx";
+import { notFound } from "next/navigation";
+import type { Metadata } from "next";
 
-export default async function Page(props: PageProps<'/docs/[[...slug]]'>) {
+export default async function Page(
+  props: PageProps<"/[[...slug]]">
+) {
   const params = await props.params;
   const page = source.getPage(params.slug);
   if (!page) notFound();
@@ -38,7 +40,7 @@ export async function generateStaticParams() {
 }
 
 export async function generateMetadata(
-  props: PageProps<'/docs/[[...slug]]'>,
+  props: PageProps<"/[[...slug]]">
 ): Promise<Metadata> {
   const params = await props.params;
   const page = source.getPage(params.slug);
@@ -52,3 +54,4 @@ export async function generateMetadata(
     },
   };
 }
+
--- lib/source.ts
+++ lib/source.ts
@@ -5,7 +5,7 @@ import { openapiPlugin } from "fumadocs-openapi/server";
 
 // See https://fumadocs.dev/docs/headless/source-api for more info
 export const source = loader({
-  baseUrl: "/docs",
+  baseUrl: "/",
   source: docs.toFumadocsSource(),
   plugins: [lucideIconsPlugin(), openapiPlugin()],
 });
