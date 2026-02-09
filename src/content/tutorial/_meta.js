92846edce8d2ec4fc2eecb6ee83778fdda3faace
Denis Fadeev <denis@fadeev.org>
2026-02-09 11:01:13 +0300

    fix: remove outdated memory retrieval and tools documentation; update useChatStorage initialization

--- src/content/tutorial/_meta.js
+++ src/content/tutorial/_meta.js
@@ -1,8 +1,6 @@
 export default {
   README: "Getting Started",
   useChatStorage: "Sending Messages",
-  useChat: "Memory Retrieval",
-  useTools: "Managing Tools",
   useModels: "Fetching Models",
   useOCR: "Extracting Text from Images",
 };
